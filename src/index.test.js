72fdfef9c8c5c7206de7106c2c798f3b80ee429c
Alan Hussey <alan.hussey@gmail.com>
2018-11-09 19:44:15 -0800

    test: 💍 add integration test for index.js as an example

--- src/index.test.js
+++ src/index.test.js
@@ -9,3 +9,43 @@ test('has the expected exports', () => {
     createUnitSystem: require('./createUnitSystem'),
   });
 });
+
+describe('createUnitSystem', () => {
+  const { createUnitSystem, conversion } = index;
+  test('creates a functional unit system', () => {
+    const { createUnit, m, convert } = createUnitSystem();
+
+    const kilometer = createUnit('kilometer', {
+      alias: 'km',
+    });
+    const meter = createUnit('meter', {
+      convert: {
+        from: [kilometer, conversion.multiplyBy(1000)],
+      },
+    });
+    const centimeter = createUnit('centimeter', {
+      convert: {
+        from: [meter, conversion.multiplyBy(100)],
+      },
+    });
+
+    const inch = createUnit('inch', {
+      convert: {
+        to: [centimeter, conversion.multiplyBy(2.54)],
+      },
+    });
+    const foot = createUnit('foot', {
+      convert: {
+        from: [inch, conversion.divideBy(12)],
+      },
+    });
+    const mile = createUnit('mile', {
+      alias: 'mile',
+      convert: {
+        to: [foot, conversion.multiplyBy(5280)],
+      },
+    });
+
+    expect(convert(m(1, mile), kilometer)).toEqual(m`1.609344 km`);
+  });
+});
