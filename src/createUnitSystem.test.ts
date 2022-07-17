711848405d599f475f3f2740acdb554363606b89
Alan Hussey <alan.hussey@gmail.com>
2022-07-17 16:03:33 -0700

    unit systems can now be stitched together

--- src/createUnitSystem.test.ts
+++ src/createUnitSystem.test.ts
@@ -11,4 +11,46 @@ describe(createUnitSystem, () => {
     expect(measurement.value).toBe(12);
     expect(measurement.unit).toBe(inches);
   });
+
+  it('can clone an existing unit system', () => {
+    const inches = new Unit('inch');
+    const feet = new Unit('foot');
+    const miles = new Unit('mile');
+    const oldMeasure = createUnitSystem`
+      ${miles} * 5280 -> ${feet}
+      ${feet} * 12 -> ${inches}
+    `;
+
+    const measure = createUnitSystem`
+      ${oldMeasure}
+    `;
+
+    expect(measure(1, miles).in(inches)).toEqual(measure(5280 * 12, inches));
+  });
+
+  it('can stitch together multiple unit systems', () => {
+    const ms = new Unit('meter');
+    const cms = new Unit('centimeter');
+    const kms = new Unit('kilometer');
+    const measureSI = createUnitSystem`
+      ${ms} * 100 -> ${cms}
+      ${ms} / 1000 -> ${kms}
+    `;
+
+    const inches = new Unit('inch');
+    const feet = new Unit('foot');
+    const miles = new Unit('mile');
+    const measureUS = createUnitSystem`
+      ${miles} * 5280 -> ${feet}
+      ${feet} * 12 -> ${inches}
+    `;
+
+    const measure = createUnitSystem`
+      ${measureSI}
+      ${measureUS}
+      ${inches} * 2.54 -> ${cms}
+    `;
+
+    expect(measure(1, miles).in(kms)).toEqual(measure(1.609344, kms));
+  });
 });
--- src/createUnitSystem.ts
+++ src/createUnitSystem.ts
@@ -3,18 +3,50 @@ import Converters from './Converters';
 import Unit from './Unit';
 import parseConversions from './parseConversions';
 
-export type MeasureFn = (value: number, unit: Unit) => Measurement;
+const __SECRET_INTERNALS_DO_NOT_USE = Symbol('secret unit-system internals');
+
+type MeasureFn = {
+  (value: number, unit: Unit): Measurement;
+  [__SECRET_INTERNALS_DO_NOT_USE]: Converters;
+};
 
 export const createMeasure = (converters: Converters): MeasureFn =>
-  function measure(value: number, unit: Unit) {
-    return new Measurement(value, unit, converters);
-  };
+  Object.assign(
+    function measure(value: number, unit: Unit) {
+      return new Measurement(value, unit, converters);
+    },
+    { [__SECRET_INTERNALS_DO_NOT_USE]: converters },
+  );
+
+const isMeasureFn = (arg: unknown): arg is MeasureFn =>
+  typeof arg === 'function' &&
+  arg.hasOwnProperty(__SECRET_INTERNALS_DO_NOT_USE);
+
+function* chain<T>(...generators: Generator<T>[]): Generator<T> {
+  for (const generator of generators) {
+    yield* generator;
+  }
+}
+
+function getAllConversions(
+  fragments: Parameters<typeof parseConversions>[0],
+  ...args: (Unit | MeasureFn)[]
+) {
+  const conversions = parseConversions(fragments, ...args);
+  const measures = args.filter(isMeasureFn);
+  return chain(
+    conversions,
+    ...measures.map((measure) =>
+      measure[__SECRET_INTERNALS_DO_NOT_USE][Symbol.iterator](),
+    ),
+  );
+}
 
 export function createUnitSystem(
-  ...args: Parameters<typeof parseConversions>
+  ...args: Parameters<typeof getAllConversions>
 ): MeasureFn {
-  const conversions = parseConversions(...args);
-  const converters = new Converters(conversions);
+  const allConversions = getAllConversions(...args);
+  const converters = new Converters(allConversions);
   const measure = createMeasure(converters);
   return measure;
 }
